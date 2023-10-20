bd3f5172a0bfa9a8af07e2e629392f07b18eb641
dieum512 <akonkwadieum222@gmail.com>
2023-10-20 19:05:25 +0200

    Import the Experience component into the app file

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav';
 import About from './components/about/About'
+import Experience from './components/experience/Experiene'
 
 const App = () => {
   return (
     <>
       <Header />
       <Nav />
-      <About/>
+      <About />
+      <Experience/>
     </>
   )
 }

8bd1a9a8c577dfcd8ab5a836e1c2246efea5076b
dieum512 <akonkwadieum222@gmail.com>
2023-10-20 19:06:43 +0200

    Import the Services component into the app file

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/header/Header'
 import Nav from './components/nav/Nav';
 import About from './components/about/About'
 import Experience from './components/experience/Experiene'
+import Services from './components/services/Services'
 
 const App = () => {
   return (
@@ -10,7 +11,8 @@ const App = () => {
       <Header />
       <Nav />
       <About />
-      <Experience/>
+      <Experience />
+      <Services/>
     </>
   )
 }

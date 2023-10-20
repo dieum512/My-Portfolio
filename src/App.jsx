2d71c3ddb94f63a68b2da3672e38de98d2166fab
dieum512 <akonkwadieum222@gmail.com>
2023-10-20 19:12:33 +0200

    Import the Footer component into the app file

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
 import Testimonials from './components/testimonials/Testimonials'
 import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
@@ -18,7 +19,8 @@ const App = () => {
       <Services />
       <Portfolio />
       <Testimonials />
-      <Contact/>
+      <Contact />
+      <Footer/>
     </>
   )
 }

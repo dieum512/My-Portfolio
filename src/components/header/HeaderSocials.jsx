d31c4384a5892a7385ada380149f52cfeb5b79bc
dieum512 <akonkwadieum222@gmail.com>
2023-10-25 21:21:36 +0200

    Updatae the headersocials component

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+      <a href="https://www.linkedin.com/in/dieum-akonkwa/" target='blank'><BsLinkedin/></a>
+      <a href="https://github.com/dieum512/" target='blank'></a>
+      <a href="https://twitter.com/dieumakonkw" target='blank'></a>
+    </div>
+  )
+}
+
+export default HeaderSocials 
--- src/components/header/headerSocials.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const headerSocials = () => {
-  return (
-    <div className='header__socials'>headerSocials</div>
-  )
-}
-
-export default headerSocials

7ea189ce771b42b83a828b8a44de40e7cfd52db2
dieum512 <akonkwadieum222@gmail.com>
2023-10-25 21:27:55 +0200

    Import the twiter Icon

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { BsLinkedin } from 'react-icons/bs'
-import {FaGithub} from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
+import {FaTwitter} from 'react-icons/fa'
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
       <a href="https://www.linkedin.com/in/dieum-akonkwa/" target='blank'><BsLinkedin/></a>
       <a href="https://github.com/dieum512/" target='blank'><FaGithub/></a>
-      <a href="https://twitter.com/dieumakonkw" target='blank'></a>
+      <a href="https://twitter.com/dieumakonkw" target='blank'><FaTwitter/></a>
     </div>
   )
 }

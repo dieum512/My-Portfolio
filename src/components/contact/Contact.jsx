f32718af355267703095081987039d046411a9e2
dieum512 <akonkwadieum222@gmail.com>
2023-10-31 09:12:25 +0200

    Create the article for the mail contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './contact.css';
+import { MdOutlineEmail } from 'react-icons/md';
 
 const Contact = () => {
   return (
     <section id="contact">
       <h5>Get In touch</h5>
       <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail/>
+            <h4>Email</h4>
+            <h5>akonkwadieum222@gmail.com</h5>
+            <a href="mailto:akonkwadieum222@gmail.com">Send a message</a>
+          </article>
+        </div>
+      </div>
     </section>
   )
 }

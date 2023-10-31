7957db9c31889055aad31ce053f5bd4e87851074
dieum512 <akonkwadieum222@gmail.com>
2023-10-31 09:55:44 +0200

    Add the classnames

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -12,19 +12,19 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail/>
+            <MdOutlineEmail className='contact__option-icon' />
             <h4>Email</h4>
             <h5>akonkwadieum222@gmail.com</h5>
             <a href="mailto:akonkwadieum222@gmail.com" target='_blank'>Send a message</a>
           </article>
           <article className="contact__option">
-            <AiOutlineLinkedin/>
+            <AiOutlineLinkedin className='contact__option-icon' />
             <h4>LinkedIn</h4>
             <h5>Dieum Akonkwa</h5>
             <a href="https://www.linkedin.com/in/dieumakonkwa/" target='_blank'>Connect with me on LinkedIn</a>
           </article>
           <article className="contact__option">
-            <BsWhatsapp/>
+            <BsWhatsapp className='contact__option-icon' />
             <h4>WhatsApp</h4>
             <h5>+243 976 728 689</h5>
             <a href="https://api.whatsapp.com/send?phone=+243976728689" target='_blank'>Send a message</a>

f1d3135391962d1e421b79418af87bf7909a8eee
dieum512 <akonkwadieum222@gmail.com>
2023-11-08 10:39:24 +0200

    change the portfolio icon nav emmplacement

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -10,10 +10,10 @@ const Nav = () => {
   return (
     <nav>
       <a href="#header" aria-label="Header Section" onClick={() => setActiveNav('#header')} className={activeNav === '#header' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href="#portfolio" aria-label="Portfolio Section" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><BsCodeSlash /></a>
       <a href="#about" aria-label="About Section" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
       <a href="#experience" aria-label="Experience Section" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
       <a href="#services" aria-label="Services Section" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine /></a>
-      <a href="#portfolio" aria-label="Portfolio Section" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><BsCodeSlash /></a>
       <a href="#contact" aria-label="Contact Section" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></a>
     </nav>
   );
